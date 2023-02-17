1c25a9a229e2102f6eb1b1654587dfa51722d6a9
Maksim Litviniuk <maksim.litviniuk@gmail.com>
2023-02-17 12:49:11 +0100

    test

--- src/main/java/com/litviniuk/project/EquipmentServiceApp/config/SecurityConfig.java
+++ src/main/java/com/litviniuk/project/EquipmentServiceApp/config/SecurityConfig.java
@@ -1,21 +1,42 @@
 package com.litviniuk.project.EquipmentServiceApp.config;
 
-import com.litviniuk.project.EquipmentServiceApp.security.AuthProviderImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
+import com.litviniuk.project.EquipmentServiceApp.services.AppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final AppUserDetailsService appUserDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider){
-        this.authProvider = authProvider;
+    public SecurityConfig(AppUserDetailsService appUserDetailsService) {
+        this.appUserDetailsService = appUserDetailsService;
     }
 
-    protected void configure (AuthenticationManagerBuilder authenticationManagerBuilder ) {
-        authenticationManagerBuilder.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
+
+//    private final AuthProviderImpl authProvider;
+//
+//    @Autowired
+//    public SecurityConfig(AuthProviderImpl authProvider){
+//        this.authProvider = authProvider;
+//    }
+//
+//    protected void configure (AuthenticationManagerBuilder authenticationManagerBuilder ) {
+//        authenticationManagerBuilder.authenticationProvider(authProvider);
+//    }
+//}
